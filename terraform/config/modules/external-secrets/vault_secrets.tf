48be9171b415b00fad219214a0191d799263a0e2
Mehedi Hasan Parvez <hasanmehedi185@gmail.com>
2026-04-03 16:03:58 +0600

    feat: region of secret updated

--- terraform/config/modules/external-secrets/vault_secrets.tf
+++ terraform/config/modules/external-secrets/vault_secrets.tf
@@ -31,7 +31,7 @@ spec:
   provider:
     oracle:
       vault: ${var.vault_id}
-      region: eu-frankfurt-1
+      region: ap-singapore-1
       auth:
         user: ${oci_identity_user.external_secrets.id}
         tenancy: ${var.tenancy_id}
